a69e5ad396d2c5a1009b02fbb42d94c2ceb33735
henry cunha <henrycunh@gmail.com>
2022-06-03 20:45:50 -0300

    test: separate tests and check inheritance behavior

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ poetry add fastapi-qp
 You can just define a Pydantic schema describing your query parameters, and extends the class with `QueryParam`, which you'll import from the package
 
 ```python
-# schemas/common.py
+# schemas/params/common.py
 from fastapi_qp import QueryParam
 
 class CommonParams(BaseModel, QueryParam):
@@ -35,6 +35,8 @@ def read_items(params: CommonParams = Depends(CommonParams.params())):
     return get_items(offset=params.offset, limit=params.limit)
 ```
 
+##### Testing
+
 In testing, you can use the `.to_url()` method to get a URL component with the params encoded
 
 ```python
@@ -48,6 +50,45 @@ def test_items():
     response = _get_items(CommonParams(limit=10))
     assert len(response.json()) <= 10
 ```
+
+#### Composing parameters
+You can combine different parameters together through inheritance
+
+```python
+# schemas/params/common.py
+
+class DateParams(BaseModel, QueryParam):
+    since: Optional[str] = Field(
+        None, 
+        description="A date in the format YYYY-MM-DD",
+        min_length=10,
+        max_length=10,
+        regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}$",
+    )
+    until: Optional[str] = Field(
+        None, 
+        description="A date in the format YYYY-MM-DD",
+        min_length=10,
+        max_length=10,
+        regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}$"
+    )
+
+class PaginationParams(BaseModel, QueryParam):
+    offset: int = Field(
+        0, 
+        description="Items ignored from the beginning of the list",
+        gte=0,
+    )
+    limit: int = Field(
+        100, 
+        description="Number of items per result",
+        ge=0,
+        le=1000
+    )
+
+class CommonParams(PaginationParams, DateParams):
+    pass
+``` 
 
 ### Acknowledgments
 This implementation is greatly inspired by @markus1978, on [this](https://github.com/tiangolo/fastapi/issues/318#issuecomment-691121286) issue.
--- test/test_qp.py
+++ test/test_qp.py
@@ -1,34 +1,70 @@
 from typing import Optional
 
+import pytest
 from fastapi import Depends, FastAPI
 from fastapi.testclient import TestClient
 from fastapi_qp import QueryParam
-from pydantic import Field
+from pydantic import Field, ValidationError
+from pydantic.error_wrappers import ValidationError
 from pydantic.main import BaseModel
 
 
-class Sample(BaseModel, QueryParam):
-    since: Optional[str] = Field(None, description="A date in the format YYYY-MM-DD")
-    until: Optional[str] = Field(None, description="A date in the format YYYY-MM-DD")
+class DateParams(BaseModel, QueryParam):
+    since: Optional[str] = Field(
+        None, 
+        description="A date in the format YYYY-MM-DD",
+        min_length=10,
+        max_length=10,
+        regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}$",
+    )
+    until: Optional[str] = Field(
+        None, 
+        description="A date in the format YYYY-MM-DD",
+        min_length=10,
+        max_length=10,
+        regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}$"
+    )
+
+class PaginationParams(BaseModel, QueryParam):
+    offset: int = Field(
+        0, 
+        description="Items ignored from the beginning of the list",
+        gte=0,
+    )
+    limit: int = Field(
+        100, 
+        description="Number of items per result",
+        ge=0,
+        le=1000
+    )
+
+class CommonParams(PaginationParams, DateParams):
+    pass
 
 app = FastAPI()
 
 @app.get("/")
-async def read(
-    qp: Sample = Depends(Sample.params())
-):
-    return qp.dict()
+async def read(params: CommonParams = Depends(CommonParams.params())):
+    return params.dict()
 
 client = TestClient(app)
 
-def test_read_main():
+def test_no_params():
     response = client.get("/")
     assert response.json()['since'] == None
 
-    response = client.get("/?since=1")
-    assert response.json()['since'] == '1'
+def test_single_param():
+    response = client.get("/" + CommonParams(since='2020-01-01').to_url())
+    assert response.json()['since'] == '2020-01-01'
 
-    assert Sample(since=1).since == '1'
-    assert Sample().to_url() == None
-    assert Sample(since=1).to_url() == '?since=1'
-    assert Sample(since=1, until=1).to_url() == '?since=1&until=1'
+def test_multiple_params():
+    response = client.get("/" + CommonParams(since='2020-01-01', limit=50).to_url())
+    assert response.json()['since'] == '2020-01-01'
+    assert response.json()['limit'] == 50
+
+def test_invalid_param():
+    with pytest.raises(ValidationError):
+        client.get("/" + CommonParams(limit=1001).to_url())
+
+def test_no_param_url():
+    assert CommonParams().to_url() == None
